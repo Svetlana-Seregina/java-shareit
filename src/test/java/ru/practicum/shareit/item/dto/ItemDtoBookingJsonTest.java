8ca6fe21b70aed2fece406cd6b7d5930ab3d541f
Svetlana Seregina <minunmusikka@yandex.ru>
2023-02-03 08:00:27 +0100

    Sprint 15, review 1

--- src/test/java/ru/practicum/shareit/item/dto/ItemDtoBookingJsonTest.java
+++ src/test/java/ru/practicum/shareit/item/dto/ItemDtoBookingJsonTest.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @JsonTest
--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplTest.java
@@ -191,6 +191,7 @@ class ItemRequestServiceImplTest {
 
         verify(itemRequestRepository).findAll(sortedByCreatedDesc);
     }
+    
     @Test
     void findAllBySize_whenFindByItemRequestsOwner_thenReturnEmptyList() {
         User userRequestor = new User(0L, "Alex", "alex@yandex.ru");
