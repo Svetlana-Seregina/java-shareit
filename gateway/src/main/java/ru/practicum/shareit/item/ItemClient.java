b61520b848f60ef606be8d52d10335d9d935c449
Svetlana Seregina <minunmusikka@yandex.ru>
2023-02-13 11:00:26 +0100

    Sprint 16, review 3

--- gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Service
